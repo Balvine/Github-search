285314b251ad14b2afcaba897dc6446e83a596b1
Balvine <balvy075@gmail.com>
2020-08-31 08:58:10 +0300

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,44 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms"
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchbarComponent } from './searchbar/searchbar.component';
 import { DisplayResultComponent } from './display-result/display-result.component';
+import { RequestService } from './user-request/request.service';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RoutingModule } from './routing/routing.module';
+import { hoverDirective } from './hover.directive'
+
+const routes: Routes = [
+  { path: "resultdisplay", component: DisplayResultComponent },
+  { path: "searchbar", component: SearchbarComponent },
+  { path: "", redirectTo: "/searchbar", pathMatch: "full" },
+  { path: '**', component: NotFoundComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchbarComponent,
-    DisplayResultComponent
+    DisplayResultComponent,
+    NotFoundComponent,
+    hoverDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    RoutingModule
   ],
-  providers: [],
+  providers: [RequestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
